aa447c0b17f08ee3031c54711ca4d4f7868b80a3
Semih cakmak <cakmak.semih@hotmail.com>
2023-11-16 15:02:07 +0800

    Added seeding files

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'open-uri'
+require 'json'
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movies_serialized = URI.open(url).read
+movies = JSON.parse(user_serialized)["results"]
+
+movies.each do |movie|
+  Movie.create(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: movie["poster_path"],
+    rating: movie["vote_average"]
+  )
+
+  puts "Total movies = #{Movie.count}."
+end
