baacdaaa4dcf022229c2a8367725d8d4eb9b96ea
Semih cakmak <cakmak.semih@hotmail.com>
2023-11-16 12:54:40 +0800

    Added Validations and Associations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  validates_uniqueness_of :movie, scope: :list
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,6 @@
 class Movie < ApplicationRecord
-  
+  has_many :bookmarks
+
+  validates :name, presence: true, uniqueness: true
+  validates :overview, presence: true
 end
