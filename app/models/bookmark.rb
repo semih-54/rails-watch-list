5aeea782d1b7b691795278ff2bfa594a586d28a9
Semih cakmak <cakmak.semih@hotmail.com>
2023-11-16 13:02:29 +0800

    Added bookmark_comment validation

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,4 +3,5 @@ class Bookmark < ApplicationRecord
   belongs_to :list
 
   validates_uniqueness_of :movie, scope: :list
+  validates :comment, length: { minimum: 6 }
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
 
-  validates :name, presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
 end
